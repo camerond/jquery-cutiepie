2c378e9cd1958d2de4d2163f8017326c83f136b9
Cameron Daigle <camerondaigle@gmail.com>
2011-08-01 13:20:08 -0400

    Add automatic color spectrum generation

--- public/javascript/jquery.cherrypie.js
+++ public/javascript/jquery.cherrypie.js
@@ -38,7 +38,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
       },
       slice: {
         stroke_width: 2,
-        colors: ["#e42929"]
+        color: "#e42929"
       }
     };
     return this.each(function() {
@@ -90,7 +90,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
         x1 = x + r,
         y1 = y,
         pi = Math.PI,
-        colors = opts.slice.colors,
+        colors = typeof opts.slice.color === "string" ? getSpectrum.call(cherrypie) : [opts.slice.color],
         long_arc = 0,
         rad = 0,
         x2, y2;
@@ -103,7 +103,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
       long_arc = cherrypie.values[i].rad > pi ? 1 : 0;
       x2 = x + Math.cos(rad) * r;
       y2 = y - Math.sin(rad) * r;
-      fill_color = opts.slice.colors[i] ? opts.slice.colors[i] : opts.slice.colors[0];
+      fill_color = colors[i] ? colors[i] : colors[0];
       cherrypie.graph.path(["M", x, y, "L", x1, y1, "A", r, r, 0, long_arc, 0, x2, y2, "z"]).attr({
         fill: fill_color,
         stroke: opts.stroke,
@@ -128,5 +128,80 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
     }
     return values;
   }
+
+  function getSpectrum() {
+    var cherrypie = this,
+        color = hexToHsv(cherrypie.opts.slice.color),
+        interval = 255 / cherrypie.values.length,
+        spectrum = [cherrypie.opts.slice.color],
+        hue = color[0];
+    for (var i = 0, max = cherrypie.values.length; i < max; i++) {
+      hue += interval;
+      hue > 255 ? hue -= 255 : false;
+      color[0] = hue;
+      spectrum.push(hsvToHex(color));
+    }
+    return spectrum;
+  }
+
+  // adapted from http://matthaynes.net/blog/2008/08/07/javascript-colour-functions/
+
+  function hsvToHex(hsv) {
+      var h = hsv[0],
+          s = hsv[1] / 100,
+          v = hsv[2] / 100,
+          hi = Math.floor((h/60) % 6),
+          f = (h / 60) - hi,
+          p = v * (1 - s),
+          q = v * (1 - f * s),
+          t = v * (1 - (1 - f) * s),
+          rgb = [];
+      switch (hi) {
+          case 0: rgb = [v,t,p]; break;
+          case 1: rgb = [q,v,p]; break;
+          case 2: rgb = [p,v,t]; break;
+          case 3: rgb = [p,q,v]; break;
+          case 4: rgb = [t,p,v]; break;
+          case 5: rgb = [v,p,q]; break;
+      }
+      var r = Math.round(rgb[0]*255).toString(16),
+          g = Math.round(rgb[1]*255).toString(16),
+          b = Math.round(rgb[2]*255).toString(16);
+      r = r.length < 2 ? "0" + r : r;
+      g = g.length < 2 ? "0" + g : g;
+      b = b.length < 2 ? "0" + b : b;
+      return "#" + r + g + b;
+  }
+
+  function hexToHsv(hex) {
+      var r = parseInt(hex.substring(1, 3), 16) / 255,
+          g = parseInt(hex.substring(3, 5), 16) / 255,
+          b = parseInt(hex.substring(5, 7), 16) / 255;
+      var min = Math.min(Math.min(r, g), b),
+          max = Math.max(Math.max(r, g), b),
+          delta = max - min;
+      var value = max,
+          saturation,
+          hue;
+      if (max == min) {
+          hue = 0;
+      } else if (max == r) {
+          hue = (60 * ((g-b) / (max-min))) % 360;
+      } else if (max == g) {
+          hue = 60 * ((b-r) / (max-min)) + 120;
+      } else if (max == b) {
+          hue = 60 * ((r-g) / (max-min)) + 240;
+      }
+      if (hue < 0) {
+          hue += 360;
+      }
+      if (max == 0) {
+          saturation = 0;
+      } else {
+          saturation = 1 - (min/max);
+      }
+      return [Math.round(hue), Math.round(saturation * 100), Math.round(value * 100)];
+  }
+
 
 })(jQuery);
